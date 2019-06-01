c05aa6429e6f1818373d062204893f2d79b4e7a9
Cubeyful <cubeyful@gmail.com>
2019-06-01 16:15:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var color = [
+  "white",
+  "red",
+  "black",
+  "blue",
+  "yellow",
+  "green",
+  "cyan"
+];
+
+var colorred = "\x1b[31m";
+var colorblue = "\x1b[34m";
+var coloryellow = "\x1b[33m";
+var colorgreen = "\x1b[32m";
+var colorcyan = "\x1b[36m";
+var colornormal = "";
+var colorreset = "\x1b[0m"; // DON'T FORGET RESET FONT
+
+function pnt(text, color) {
+  if (color === undefined) color = cNormal;
+  var timestamp = "[" + new Date().toLocaleTimeString('en-CA') + "] ";
+  colorLog = color;
+  logText = text;
+  console.log(timestamp + colorLog + text + cReset);
+}
+
+function reset(message, member){
+
+  var arrayLength = color.length;
+  for (var i = 0; i < arrayLength; i++) {
+      //Do something
+      //let roleArray = message.guild.roles.find("name", rolesColors[i]);
+      var roleArray = message.guild.roles.find(role => role.name === color[i]);
+      member.removeRole(roleArray).catch(console.error);
+    }
+}
 client.on('ready', () => {
   console.log('Hello');
   client.user.setActivity('the precious code', { type: 'WATCHING' })
@@ -21,7 +57,12 @@ client.on("message", async (message) => {
   let prefix = 'code, ';
 
   if(message.content == prefix + 'emit') {
+    if(message.author.id != '566692683838521364') return;
     client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+  }
+
+  if(message.content == prefix + 'red') {
+
   }
 });
 
