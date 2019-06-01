f3e44c489b02a6ceb8f47611b3a664b242c3314b
Cubeyful <cubeyful@gmail.com>
2019-06-01 15:46:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ client.on('ready', () => {
   console.log('Hello');
 });
 
-client.login(process.env.TOKEN)
+client.on('guildMemberAdd', (member, message) => {
+  let channel = member.guild.channels.find("name", "welcome");
+
+  let embed = new Discord.RichEmbed()
+    .setColor('GREEN')
+    .setAuthor(member.user.username + "#" +member.user.discriminator + " ("+member.user.id+")", member.user.displayAvatarURL)
+    .setTimestamp()
+    .setFooter('Member Joined')
+  channel.send(embed);
+});
+
+client.login(process.env.TOKEN);
