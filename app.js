feb2f6c72cb4fd3be71608e5c29a4dc0695fa64d
Cubeyful <cubeyful@gmail.com>
2019-06-01 21:12:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -70,67 +70,77 @@ client.on("message", async (message) => {
   //   client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
   // }
 
-  if(message.content == prefix + 'application') {
-    let embed = new Discord.RichEmbed()
-      .setColor('YELLOW')
-      .setTimestamp()
-      .setFooter('Application Handler')
-      .setDescription(`◀ Developer)`)
-      .setAuthor(message.author.username, message.author.displayAvatarURL);
-    message.channel.send(embed);
-    message.react('◀')
+  // if(message.content == prefix + 'application') {
+  //   let embed = new Discord.RichEmbed()
+  //     .setColor('YELLOW')
+  //     .setTimestamp()
+  //     .setFooter('Application Handler')
+  //     .setDescription(`◀ Developer)`)
+  //     .setAuthor(message.author.username, message.author.displayAvatarURL);
+  //   message.channel.send(embed);
+  //   message.react('◀')
 
-    const filter = (reaction, user) => {
-      return ['◀'].includes(reaction.emoji.name) && user.id === message.author.id;
-    };
+  //   const filter = (reaction, user) => {
+  //     return ['◀'].includes(reaction.emoji.name) && user.id === message.author.id;
+  //   };
 
-    message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-    .then(collected => {
-      const reaction = collected.first();
+  //   message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+  //   .then(collected => {
+  //     const reaction = collected.first();
   
-      if (reaction.emoji.name === '◀') {
-        reaction.remove();
-        let embed = new Discord.RichEmbed()
-          .setColor('GREEN')
-          .setTimestamp()
-          .setFooter(`Developer Application`)
-          .setDescription('The application has started in your direct messages, please answer honestly.')
-          .setAuthor(message.author.username, message.author.displayAvatarURL)
-        message.channel.send(embed);
+  //     if (reaction.emoji.name === '◀') {
+  //       reaction.remove();
+  //       let embed = new Discord.RichEmbed()
+  //         .setColor('GREEN')
+  //         .setTimestamp()
+  //         .setFooter(`Developer Application`)
+  //         .setDescription('The application has started in your direct messages, please answer honestly.')
+  //         .setAuthor(message.author.username, message.author.displayAvatarURL)
+  //       message.channel.send(embed);
 
-        let finish = new Discord.RichEmbed()
-          .setColor('GREEN')
-          .setTimestamp()
-          .setFooter('Application Complete')
-          .setAuthor(message.author.username, message.author.displayAvatarURL)
+  //       let finish = new Discord.RichEmbed()
+  //         .setColor('GREEN')
+  //         .setTimestamp()
+  //         .setFooter('Application Complete')
+  //         .setAuthor(message.author.username, message.author.displayAvatarURL)
 
-        let embeddm = new Discord.RichEmbed()
-          .setColor('GREEN')
-          .setTimestamp()
-          .setFooter(`Developer Application`)
-          .setDescription(`Question One\nHow old are you?`)
-          .setAuthor(message.author.username, message.author.displayAvatarURL)
-        message.author.send(embeddm);
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 30000 });
-        collector.on('collect', (msg2) => {
-          if(isNaN(msg2)) {
-            let embed = new Discord.RichEmbed()
-              .setColor('RED')
-              .setTimestamp()
-              .setFooter('Developer Application')
-              .setDescription(`Your age has to be a number.`);
-            message.author.send(embed);
-          } else {
-            finish.addField('Age', msg1.content)
-            message.author.send(finish);
-          }
-        });
-      } else {
-        message.reply('you reacted with a thumbs down.');
-      }
-    })
+  //       let embeddm = new Discord.RichEmbed()
+  //         .setColor('GREEN')
+  //         .setTimestamp()
+  //         .setFooter(`Developer Application`)
+  //         .setDescription(`Question One\nHow old are you?`)
+  //         .setAuthor(message.author.username, message.author.displayAvatarURL)
+  //       message.author.send(embeddm);
+  //       const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 30000 });
+  //       collector.on('collect', (msg2) => {
+  //         if(isNaN(msg2)) {
+  //           let embed = new Discord.RichEmbed()
+  //             .setColor('RED')
+  //             .setTimestamp()
+  //             .setFooter('Developer Application')
+  //             .setDescription(`Your age has to be a number.`);
+  //           message.author.send(embed);
+  //         } else {
+  //           finish.addField('Age', msg2.content)
+  //           message.author.send(finish);
+  //         }
+  //       });
+  //     } else {
+  //       message.reply('you reacted with a thumbs down.');
+  //     }
+  //   })
 
-  }
+  // }
+
+    if(message.content == prefix + 'reaction') {
+      if(message.author.id != '566692683838521364') return;
+      let embed = new Discord.RichEmbed()
+        .setColor('GREEN')
+        .setTimestamp()
+        .setFooter('Reaction Role')
+        .setDescription(`Choose your own roles that you code with.`)
+      message.channel.send(embed);
+    }
 
 });
 
